8c9ae581ee286fed579f11823775eaf3cbad75f3
lvyou <youlv2014@gmail.com>
2017-04-29 11:34:19 +0800

    简单js调用android代码：网页js调用android读取联系人信息+拨打电话+动态权限完成 1.首先网页js调用android获取联系人显示在页面上，此过程需要动态权限 2.打电话的动态权限

--- app/src/main/java/com/example/administrator/myapplication/webview/WebviewContactActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/webview/WebviewContactActivity.java
@@ -6,14 +6,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.example.administrator.myapplication.R;
 import com.example.administrator.myapplication.base.BaseActivity;
@@ -32,8 +33,11 @@ import butterknife.BindView;
 public class WebviewContactActivity extends BaseActivity {
 
     private static final int REQUEST_CALL_PHONE = 3003;
+    private static final int GET_CONTACTS = 3004;
     @BindView(R.id.web_contract)
     WebView web_contract;
+
+    private String phoneNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,53 +59,6 @@ public class WebviewContactActivity extends BaseActivity {
         setContentView(R.layout.activity_webview_contact);
     }
 
-    /**
-     * 自定义一个Js的业务类,传递给JS的对象就是这个,调用时直接javascript:sharp.contactlist()
-     */
-    public class SharpJs {
-
-        @JavascriptInterface
-        public void contactlist() {
-            try {
-                System.out.println("contactlist()方法执行了！");
-                final String json = buildJson(getContacts());
-                //使用如下的方法报错
-                //java.lang.RuntimeException: java.lang.Throwable: A WebView method was called on thread 'JavaBridge'.
-                // All WebView methods must be called on the same thread
-//                web_contract.loadUrl("javascript:show('" + json + "')");
-                web_contract.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        web_contract.loadUrl("javascript:show('" + json + "')");
-                    }
-                });
-            } catch (Exception e) {
-                System.out.println("设置数据失败" + e);
-            }
-        }
-
-        @JavascriptInterface
-        public void call(String phone) {
-            System.out.println("call()方法执行了！");
-            Intent it = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                int callPhonePermission = ContextCompat.checkSelfPermission(WebviewContactActivity.this
-                        , Manifest.permission.CALL_PHONE);
-                if (callPhonePermission != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(WebviewContactActivity.this,
-                            new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
-                    return;
-                } else {
-                    startActivity(it);
-                }
-            } else {
-                startActivity(it);
-            }
-
-        }
-
-    }
-
     //将获取到的联系人集合写入到JsonObject对象中,再添加到JsonArray数组中
     public String buildJson(List<Contact> contacts) throws Exception {
         JSONArray array = new JSONArray();
@@ -117,6 +74,7 @@ public class WebviewContactActivity extends BaseActivity {
 
     //定义一个获取联系人的方法,返回的是List<Contact>的数据
     public List<Contact> getContacts() {
+
         List<Contact> Contacts = new ArrayList<Contact>();
         //①查询raw_contacts表获得联系人的id
         ContentResolver resolver = getContentResolver();
@@ -133,5 +91,114 @@ public class WebviewContactActivity extends BaseActivity {
         }
         cursor.close();
         return Contacts;
+    }
+
+    private void checkContractPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, GET_CONTACTS);
+        } else {
+            showContracts();
+        }
+    }
+
+    private void showContracts() {
+        try {
+            System.out.println("contactlist()方法执行了！");
+            final String json = buildJson(getContacts());
+            //使用如下的方法报错
+            //java.lang.RuntimeException: java.lang.Throwable: A WebView method was called on thread 'JavaBridge'.
+            // All WebView methods must be called on the same thread
+//                web_contract.loadUrl("javascript:show('" + json + "')");
+            web_contract.post(new Runnable() {
+                @Override
+                public void run() {
+                    web_contract.loadUrl("javascript:show('" + json + "')");
+                }
+            });
+        } catch (Exception e) {
+            System.out.println("设置数据失败" + e);
+        }
+    }
+
+    /**
+     * 自定义一个Js的业务类,传递给JS的对象就是这个,调用时直接javascript:sharp.contactlist()
+     */
+    public class SharpJs {
+
+        @JavascriptInterface
+        public void contactlist() {
+            checkContractPermission();
+
+        }
+
+        @JavascriptInterface
+        public void call(String phone) {
+            System.out.println("call()方法执行了！");
+            phoneNum = phone;
+            //Intent it = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
+           /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int callPhonePermission = ContextCompat.checkSelfPermission(WebviewContactActivity.this
+                        , Manifest.permission.CALL_PHONE);
+                if (callPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(WebviewContactActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
+                    return;
+                } else {
+                    startActivity(it);
+                }
+            } else {
+                startActivity(it);
+            }*/
+
+            checkPermission();
+
+        }
+
+    }
+
+    /**
+     * 检查权限
+     */
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
+        } else {
+            callPhone(phoneNum);
+        }
+    }
+
+    private void callPhone(String phone) {
+        Intent it = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
+        startActivity(it);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        doNext(requestCode, grantResults);
+    }
+
+    private void doNext(int requestCode, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CALL_PHONE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callPhone(phoneNum);
+                } else {
+                    Toast.makeText(this, "没有打电话权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case GET_CONTACTS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    showContracts();
+                } else {
+                    Toast.makeText(this, "没有联系人权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
