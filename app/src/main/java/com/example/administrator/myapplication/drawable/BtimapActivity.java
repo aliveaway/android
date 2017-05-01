1a421522e4a701e8a7fd42fda6c1277f53f4ce41
lvyou <youlv2014@gmail.com>
2017-05-01 12:00:05 +0800

    bitmap简单试用：为啥一个32k的图加载后内存升到32M

--- app/src/main/java/com/example/administrator/myapplication/drawable/BtimapActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/drawable/BtimapActivity.java
@@ -1,10 +1,14 @@
 package com.example.administrator.myapplication.drawable;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.administrator.myapplication.R;
@@ -21,7 +25,10 @@ import butterknife.OnClick;
 public class BtimapActivity extends BaseActivity {
 
     @BindView(R.id.capture_btn)
-    Button img_capture;
+    Button capture_btn;
+
+    @BindView(R.id.img_capture)
+    ImageView img_capture;
 
     static ByteArrayOutputStream byteOut = null;
     private Bitmap bitmap = null;
@@ -30,7 +37,72 @@ public class BtimapActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setBitmapFromResources();
 
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+        // 第一次解析将inJustDecodeBounds设置为true，来获取图片大小
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        // 调用上面定义的方法计算inSampleSize值
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        // 使用获取到的inSampleSize值再次解析图片
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    /**
+     * 计算宽高比率
+     *
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        // 源图片的高度和宽度
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            // 计算出实际宽高和目标宽高的比率
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            // 选择宽和高中最小的比率作为inSampleSize的值，这样可以保证最终图片的宽和高
+            // 一定都会大于等于目标的宽和高。
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        return inSampleSize;
+    }
+
+    //获取APP最大分配内存
+    private void getMaxMem() {
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        Log.d("TAG", "Max memory is " + maxMemory + "KB");
+    }
+
+    private void getImgInfo(int resId) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), resId, options);
+        int imgWidth = options.outWidth;
+        int imgHeight = options.outHeight;
+        String imgType = options.outMimeType;
+        Log.i("TAG", "图片宽度" + imgWidth);
+        Log.i("TAG", "图片高度" + imgHeight);
+        Log.i("TAG", "图片类型" + imgType);
+    }
+
+
+    private void setBitmapFromResources() {
+//        final View contentView = getWindow().getDecorView();
+        Bitmap b = decodeSampledBitmapFromResource(getResources(), R.drawable.meinv,
+                img_capture.getWidth(), img_capture.getHeight());
+        img_capture.setImageBitmap(b);
     }
 
     @Override
@@ -93,5 +165,16 @@ public class BtimapActivity extends BaseActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        img_capture.srcCompat();
+        if (null != bitmap && !bitmap.isRecycled()) {
+            bitmap.recycle();
+            bitmap = null;
+        }
+        img_capture.setImageBitmap(null);
     }
 }
